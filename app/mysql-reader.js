8b7b55fdb59fe19971f0e81b5c330e88c302c447
jcardus <asklocation.net@gmail.com>
2024-01-10 21:20:16 +0000

    mysql lazy connect

--- app/mysql-reader.js
+++ app/mysql-reader.js
@@ -1,14 +1,17 @@
 const mysql = require('mysql2/promise');
 
-const pool = mysql.createConnection({
-  host: process.env.DB_HOST_READER,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
-});
+let conn
 
 async function getRows(sql) {
-  return (await pool).query(sql);
+  if (!conn) {
+    conn = mysql.createConnection({
+      host: process.env.DB_HOST_READER,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE
+    });
+  }
+  return (await conn).query(sql);
 }
 
 exports.getRows = getRows;
