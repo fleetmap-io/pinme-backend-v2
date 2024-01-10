a71b7fb07925349fb96f93e7ad1ff8cd41ba9e10
jcardus <asklocation.net@gmail.com>
2024-01-10 18:56:04 +0000

    secrets full access

--- app/api/google.js
+++ app/api/google.js
@@ -1,7 +1,10 @@
 const { getSecretValue } = require('../secrets')
 const { sign } = require('./url-signer')
-const _secret = getSecretValue('GOOGLE_API_KEY')
+let _secret
 exports.getImageUrl = async (center) => {
+  if (!secret) {
+    _secret = getSecretValue('GOOGLE_API_KEY')
+  }
   const secret = await _secret
   const url = `https://maps.googleapis.com/maps/api/staticmap?maptype=hybrid&markers=${center}&center=${center}&zoom=14&size=700x500&client=gme-fleetmatics`
   return sign(url, secret.GOOGLE_API_SECRET_KEY)
