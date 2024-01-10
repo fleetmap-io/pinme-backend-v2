5820bf4724d9d05306c479b0bb2ee433bd5b87fe
jcardus <asklocation.net@gmail.com>
2024-01-10 19:07:40 +0000

    add files

---
+++ app/event/driverChanged.js
@@ -0,0 +1,43 @@
+const traccar = require('../api/traccar')
+const { getDriver } = require('../drivers')
+const { logException } = require('../utils')
+
+async function processDriverChanged (event, retries = 3) {
+  const date = new Date()
+  try {
+    if (event.position) {
+      if (!event.position.attributes.driverUniqueId || event.position.attributes.driverUniqueId === '0') {
+        return
+      }
+
+      const driver = await getDriver(event.position.attributes.driverUniqueId)
+
+      if (driver) {
+        if (driver.attributes.deviceId !== event.position.deviceId) {
+          console.log('update driver', driver.name, 'from device id', driver.attributes.deviceId, 'to', event.position.deviceId)
+          driver.attributes.deviceId = event.position.deviceId
+          await traccar.updateDriver(driver).then(d => d.data)
+        }
+      }
+
+      const device = event.device
+      if (device) {
+        if (device.attributes.lastDriverUniqueId !== event.position.attributes.driverUniqueId) {
+          device.attributes.lastDriverUniqueId = event.position.attributes.driverUniqueId
+          await traccar.updateDevice(device.id, device).then(d => d.data)
+        }
+      }
+    } else {
+      console.log('ignoring driverChanged, no position', event)
+    }
+  } catch (e) {
+    if (e.response && e.response.data && e.response.data.startsWith('ConcurrentModificationException') && retries) {
+      console.warn('concurrent exception trying again', retries, event.event)
+      return processDriverChanged(event, retries--)
+    } else {
+      logException(e, 'processDriverChanged', event.event && event.event.attributes, 'after', new Date()-date, 'ms')
+    }
+  }
+}
+
+exports.processDriverChanged = processDriverChanged
---
+++ app/event/ignition.js
@@ -0,0 +1,35 @@
+const dynamo = require('../api/dynamo')
+const traccar = require('../api/traccar')
+const { logException } = require('../utils')
+
+const driverAppCompanies = [1554, 1159, 2579, 961, 1086]
+
+async function processIgnitionOff (event) {
+  try {
+    if (event.position) {
+      const deviceId = event.device.id
+      const positionDate = event.position.fixTime
+      await dynamo.updateIgnitionOff(deviceId, positionDate)
+      if (driverAppCompanies.find(id => parseInt(event.device.attributes.clientId) === id)) {
+        if (!event.users.some(u => u.attributes.driverAppHoldDriver)) {
+          if (event.device.attributes.deviceType !== 1) {
+            console.log('sending setparam to', event.device.name)
+            await traccar.sendCommand(deviceId, event.device.attributes.buzzer ? 'setparam 11700:3' : 'setparam 11700:0')
+          }
+          if (event.device.attributes.driverUniqueId) {
+            const [device] = await traccar.getDevicesById([deviceId])
+            console.log('Logout driver', device.name, device.attributes.driverUniqueId)
+            delete device.attributes.driverUniqueId
+            await traccar.updateDevice(deviceId, device)
+          }
+        }
+      }
+    } else {
+      console.log('processIgnitionOff ignoring event without position', event)
+    }
+  } catch (e) {
+    logException('processIgnitionOff', event.device && event.device.name)
+  }
+}
+
+exports.processIgnitionOff = processIgnitionOff
