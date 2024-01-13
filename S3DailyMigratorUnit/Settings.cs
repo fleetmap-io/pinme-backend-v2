836f5667faa70d4ce95d6696fc746e1ad3c148a0
jcardus <asklocation.net@gmail.com>
2024-01-13 01:46:47 +0000

    S3DailyMigratorUnit.csproj

---
+++ S3DailyMigratorUnit/Settings.cs
@@ -0,0 +1,13 @@
+﻿namespace S3DailyMigratorUnit
+{
+    public static class Settings
+    {
+        public static string mysqlcs = "server=dbpositions.pinme.io;database=traccar;userid=s3dailymigratorunit;password=s3d41lym1gr4t0r5n1t;maxpoolsize=1";
+
+        public static string mysqlcsread = "server=dbpositions-read.pinme.io;database=traccar;userid=s3dailymigratorunit;password=s3d41lym1gr4t0r5n1t;maxpoolsize=1";
+
+        public static string bucket = "traccar-rds-archived-v3";
+
+        public static int sleepTime = 5000;
+    }
+}
