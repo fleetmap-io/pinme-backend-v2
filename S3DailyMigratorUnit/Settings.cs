6adf3f9c0a6bdb67975e43f4656d92b4d1cd3050
jcardus <asklocation.net@gmail.com>
2024-01-14 20:18:12 +0000

    db

--- S3DailyMigratorUnit/Settings.cs
+++ S3DailyMigratorUnit/Settings.cs
@@ -2,9 +2,9 @@
 {
     public static class Settings
     {
-        public static string mysqlcs = "server=dbpositions.pinme.io;database=traccar;userid=s3dailymigratorunit;password=s3d41lym1gr4t0r5n1t;maxpoolsize=1";
+        public static string mysqlcs = $"server={Environment.GetEnvironmentVariable("DB_HOST_POSITIONS")};database=traccar;userid={Environment.GetEnvironmentVariable("DB_USER")};password={Environment.GetEnvironmentVariable("DB_PASSWORD")};maxpoolsize=1";
 
-        public static string mysqlcsread = "server=dbpositions-read.pinme.io;database=traccar;userid=s3dailymigratorunit;password=s3d41lym1gr4t0r5n1t;maxpoolsize=1";
+        public static string mysqlcsread = $"server={Environment.GetEnvironmentVariable("DB_HOST_POSITIONS_READER")};database=traccar;userid={Environment.GetEnvironmentVariable("DB_USER")};password={Environment.GetEnvironmentVariable("DB_PASSWORD")};maxpoolsize=1";
 
         public static string bucket = "traccar-rds-archived-v3";
 
