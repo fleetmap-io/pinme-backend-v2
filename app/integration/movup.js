a2c8ccf11cea9610712c9d1728db324c082e6735
jcardus <asklocation.net@gmail.com>
2024-01-16 04:45:34 +0000

    working

--- app/integration/movup.js
+++ app/integration/movup.js
@@ -1,3 +1,12 @@
+const axios = require('axios')
 module.exports = async (e) => {
-    await require("./sitrans")(e)
+    await require("./sitrans")(e, 'Basic RmxlZXRyYWNrOnlZb0htR2tFN21Yag==', 'https://interop.altomovup.com/gpssignal/api/v1/data/ulog-cl')
+    console.log(
+        await axios.post('https://segmentado.ziyu.cl/api/restapp/gpssignal/external/', e,
+            {
+                headers: {
+                    Username: 'Fleetrack',
+                    Password: 'yYoHmGkE7mXj'
+                }
+            }).then(d => d.data))
 }
