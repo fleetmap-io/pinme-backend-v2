8839ddfef43b8a013aa6ee17f18fc7e8f9834ce5
aws-us-east <155696574+aws-us-east@users.noreply.github.com>
2024-01-16 02:06:41 +0000

    log error

--- app/mysql.js
+++ app/mysql.js
@@ -1,14 +1,14 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2/promise')
 
-let conn = {}
+const conn = {}
 
 const user = process.env.DB_USER
 const password = process.env.DB_PASSWORD
 const database = process.env.DB_DATABASE
 
-function initConn(host, retries = 10) {
-  return mysql.createConnection({host, user, password, database}).catch(e => {
-    console.error(e)
+function initConn (host, retries = 10) {
+  return mysql.createConnection({ host, user, password, database }).catch(e => {
+    console.error('connection error', host, e.message, 'try again', retries)
     if (retries--) {
       return initConn(host, retries)
     } else {
@@ -17,16 +17,16 @@ function initConn(host, retries = 10) {
   })
 }
 
-async function getRows(sql, host = process.env.DB_HOST) {
+async function getRows (sql, host = process.env.DB_HOST) {
   if (!conn[host]) {
-    conn[host] = initConn(host);
+    conn[host] = initConn(host)
   }
-  return (await conn[host]).query(sql);
+  return (await conn[host]).query(sql)
 }
 
-exports.getRows = getRows;
+exports.getRows = getRows
 
 exports.getRowsArray = async (sql, host = process.env.DB_HOST) => {
-  const [result] = await getRows(sql, host);
-  return result;
-};
+  const [result] = await getRows(sql, host)
+  return result
+}
