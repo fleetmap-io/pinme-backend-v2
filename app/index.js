551195180c8eb237dbc28b15d9bef4488d799813
jcardus <asklocation.net@gmail.com>
2024-01-16 03:06:13 +0000

    ups

--- app/index.js
+++ app/index.js
@@ -2,6 +2,6 @@ const { pushEvents } = require('./event')
 const { pushPositions } = require('./push')
 exports.eventsAndPositionsConsumer = (e, context) => {
   console.log(context, e.Records.length, 'records')
-  return Promise.all(e.Records.map(event => event.eventSourceARN === process.env.QUEUE_EVENTS ? pushEvents(event)
-      : pushPositions(event)))
+  return Promise.all(e.Records.map(event => event.eventSourceARN === process.env.QUEUE_POSITIONS ? pushPositions(event)
+      : pushEvents(event)))
 }
