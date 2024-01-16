22359ef3331bac0aded04757a98308d3363836fb
jcardus <asklocation.net@gmail.com>
2024-01-16 03:39:31 +0000

    QUEUE_LOCATIONS_ARN

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ const { pushEvents } = require('./event')
 const { pushPositions } = require('./push')
 exports.eventsAndPositionsConsumer = (e) => {
   console.log(e.Records.length, 'records')
-  return Promise.all(e.Records.map(event => event.eventSourceARN === process.env.QUEUE_POSITIONS_ARN ? pushPositions(event)
+  return Promise.all(e.Records.map(event => event.eventSourceARN === process.env.QUEUE_LOCATIONS_ARN
+      ? pushPositions(event)
       : pushEvents(event)))
 }
