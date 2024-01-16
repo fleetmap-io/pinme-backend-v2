215399b962950803eff3bad1be8d7035c789a695
jcardus <asklocation.net@gmail.com>
2024-01-16 00:50:40 +0000

    init connection

--- app/mysql.js
+++ app/mysql.js
@@ -2,14 +2,24 @@ const mysql = require('mysql2/promise');
 
 let conn = {}
 
+const user = process.env.DB_USER
+const password = process.env.DB_PASSWORD
+const database = process.env.DB_DATABASE
+
+function initConn(host, retries = 10) {
+  return mysql.createConnection({host, user, password, database}).catch(e => {
+    console.error(e)
+    if (retries--) {
+      return initConn(host, retries)
+    } else {
+      throw e
+    }
+  })
+}
+
 async function getRows(sql, host = process.env.DB_HOST) {
   if (!conn[host]) {
-    conn[host] = mysql.createConnection({
-      host,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE
-    })
+    initConn(host);
   }
   return (await conn[host]).query(sql);
 }
