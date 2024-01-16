981758ec4e508e86a303d40df8cd81306738d6dc
aws-us-east <155696574+aws-us-east@users.noreply.github.com>
2024-01-16 02:26:11 +0000

    log

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 const { pushEvents } = require('./event')
 exports.eventsAndPositionsConsumer = (e) => {
-  console.log(e.Records, 'records')
+  console.log(e, e.Records.length, 'records')
   return Promise.all(e.Records.map(event => pushEvents(event)))
 }
