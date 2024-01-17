2279bbfe34eb9b82d829d883024aca7ebdcb0082
jcardus <asklocation.net@gmail.com>
2024-01-17 11:03:00 +0000

    fix exception

--- app/mysql.js
+++ app/mysql.js
@@ -6,16 +6,18 @@ const user = process.env.DB_USER
 const password = process.env.DB_PASSWORD
 const database = process.env.DB_DATABASE
 
-function initConn (host, retries = 10) {
-  return mysql.createConnection({ host, user, password, database }).catch(e => {
-    console.error('connection error', host, e.message, 'try again', retries)
-    if (retries--) {
-      return initConn(host, retries)
-    } else {
-      throw e
+async function initConn (host, retries = 10) {
+   try {
+    return await mysql.createConnection({host, user, password, database})
+  } catch(e) {
+      console.error('connection error', host, e.message, 'try again', retries)
+      if (retries--) {
+        return initConn(host, retries)
+      } else {
+        throw e
+      }
     }
-  })
-}
+  }
 
 async function getRows (sql, host = process.env.DB_HOST) {
   if (!conn[host]) {
