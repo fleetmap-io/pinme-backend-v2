edd8344bad2f5809c8a5ca30ac6e4e06262bf0a1
jcardus <asklocation.net@gmail.com>
2024-01-20 01:53:00 +0000

    log

--- app/secrets/index.js
+++ app/secrets/index.js
@@ -2,7 +2,6 @@ const { SecretsManagerClient, GetSecretValueCommand } = require('@aws-sdk/client
 const client = new SecretsManagerClient({ region: 'us-east-1' })
 
 exports.getSecretValue = async secretName => {
-  console.error('getting', secretName)
   const data = await client.send(new GetSecretValueCommand(({ SecretId: secretName })))
   // Decrypts secret using the associated KMS CMK.
   // Depending on whether the secret is a string or binary, one of these fields will be populated.
