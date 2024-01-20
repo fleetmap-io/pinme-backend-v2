577a7f71700b52dc8c208618ece4c55af2f3b462
jcardus <asklocation.net@gmail.com>
2024-01-20 01:17:13 +0000

    remove firebase

--- app/event/notifications.js
+++ app/event/notifications.js
@@ -372,14 +372,14 @@ async function processEvent (event) {
           if (users[i].attributes.firebaseToken) {
             // await sendFirebase(notification, users[i].attributes.firebaseToken, users[i].email, users[i])
           }
-          const dUser = await dynamo.get({ user: users[i].email })
+          /* const dUser = await dynamo.get({ user: users[i].email })
           if (dUser && dUser.firebaseTokens) {
             for (const t of dUser.firebaseTokens) {
               if (t !== users[i].attributes.firebaseToken) {
                 // await sendFirebase(notification, t, users[i].email)
               }
             }
-          }
+          } */
         } catch (e) {
           console.error('error in firebase / onesignal', users[i].email, users[i].attributes.firebaseToken, event.device.name, message, e)
         }
