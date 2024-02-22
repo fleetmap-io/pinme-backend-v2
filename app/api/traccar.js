3be3f3c4c1124cbfe78b946325f848dcfeab9b5d
jpenas <jppenas@gmail.com>
2024-02-22 21:11:08 +0000

    fic getComputedAttributes

--- app/api/traccar.js
+++ app/api/traccar.js
@@ -269,7 +269,7 @@ exports.updateDeviceAccumulators = (deviceId, accumulators) => {
 }
 
 exports.getComputed = async (deviceId) => {
-  return await get(`/permissions?deviceid=${deviceId}`)
+  return await get(`/attributes/computed?deviceid=${deviceId}`)
 }
 
 exports.removeComputed = (deviceId, attributeId) => {
