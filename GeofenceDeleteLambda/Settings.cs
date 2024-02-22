c606231b2606c611ad0075c3770cfdfad80be53a
j <j>
2024-02-22 13:42:39 +0000

    fix connection string

--- GeofenceDeleteLambda/Settings.cs
+++ GeofenceDeleteLambda/Settings.cs
@@ -2,7 +2,7 @@
 {
     public static class Settings
     {
-        public static string mysqlcs = $"server={Environment.GetEnvironmentVariable("DB_USER")};database=traccar;userid={Environment.GetEnvironmentVariable("DB_USER")};password={Environment.GetEnvironmentVariable("DB_PASSWORD")};maxpoolsize=1";
+        public static string mysqlcs = $"server={Environment.GetEnvironmentVariable("DB_HOST")};database=traccar;userid={Environment.GetEnvironmentVariable("DB_USER")};password={Environment.GetEnvironmentVariable("DB_PASSWORD")};maxpoolsize=1";
 
         public static string TraccarUser = Environment.GetEnvironmentVariable("TRACCAR_ADMIN_USER");
         public static string TraccarPass = Environment.GetEnvironmentVariable("TRACCAR_ADMIN_PASS");
