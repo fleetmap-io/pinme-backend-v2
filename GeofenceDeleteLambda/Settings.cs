ee73d9e62432e183eec4c48b224dbd6278d79f7d
j <j>
2024-02-22 01:38:24 +0000

    GeofenceDeleteLambda

---
+++ GeofenceDeleteLambda/Settings.cs
@@ -0,0 +1,10 @@
+﻿namespace GeofenceDeleteLambda
+{
+    public static class Settings
+    {
+        public static string mysqlcs = $"server={Environment.GetEnvironmentVariable("DB_USER")};database=traccar;userid={Environment.GetEnvironmentVariable("DB_USER")};password={Environment.GetEnvironmentVariable("DB_PASSWORD")};maxpoolsize=1";
+
+        public static string TraccarUser = Environment.GetEnvironmentVariable("TRACCAR_ADMIN_USER");
+        public static string TraccarPass = Environment.GetEnvironmentVariable("TRACCAR_ADMIN_PASS");
+    }
+}
