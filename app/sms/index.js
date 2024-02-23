3d2215e3984c91df2b46f5e9f408ac477103e602
jpenas <jppenas@gmail.com>
2024-02-23 09:58:49 +0000

    get sms and smsreceived

--- app/sms/index.js
+++ app/sms/index.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+const mysql = require('../mysql')
 
 exports.sendSms = (phone, message) => {
   return axios.get(
@@ -6,4 +7,16 @@ exports.sendSms = (phone, message) => {
         phone}&message=${
         encodeURIComponent(message.replaceAll('\'', ' '))}`
   ).then(d => d.data)
+}
+
+exports.get = async (phone) => {
+  const query = `SELECT * from traccar.sms_sent WHERE gsm = '${phone}'`
+  const [result] = await mysql.query(query)
+  return result
+}
+
+exports.getReceived = async (phone) => {
+  const query = `SELECT * from traccar.sms_received WHERE gsm = '${phone}'`
+  const [result] = await mysql.query(query)
+  return result
 }
