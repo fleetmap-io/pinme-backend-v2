f53fdb194a1edd900b5575fb0c388e51c380ccc4
jcardus <asklocation.net@gmail.com>
2024-02-23 13:44:40 +0000

    boolean

--- app/mysql.js
+++ app/mysql.js
@@ -10,7 +10,19 @@ async function getRows (sql, reader = false) {
       : process.env.DB_HOST,
     user,
     password,
-    database
+    database,
+    typeCast: function castField (field, useDefaultTypeCasting) {
+      // We only want to cast bit fields that have a single-bit in them. If the field
+      // has more than one bit, then we cannot assume it is supposed to be a Boolean.
+      if ((field.type === 'BIT') && (field.length === 1)) {
+        const bytes = field.buffer()
+        // A Buffer in Node represents a collection of 8-bit unsigned integers.
+        // Therefore, our single "bit field" comes back as the bits '0000 0001',
+        // which is equivalent to the number 1.
+        return (bytes[0] === 1)
+      }
+      return (useDefaultTypeCasting())
+    }
   })
   const result = conn.query(sql)
   await conn.end()
