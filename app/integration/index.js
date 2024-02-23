5f18f2d79991b310c1499aff23d5307a0fc5e327
jcardus <asklocation.net@gmail.com>
2024-02-23 19:17:40 +0000

    unigis.js

--- app/integration/index.js
+++ app/integration/index.js
@@ -3,4 +3,5 @@ exports.centinela = require('./centinela')
 exports.sitrans = require('./sitrans')
 exports.movup = require('./movup')
 exports.owl = require('./owl')
+exports.unigis = require('./unigis')
 exports.monitrip = async () => {}
