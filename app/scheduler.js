7ab27570fedc2569f797c86c6c645b0d3863fcc8
jpenas <jppenas@gmail.com>
2024-02-28 13:14:44 +0000

    scheduler send report

--- app/scheduler.js
+++ app/scheduler.js
@@ -5,7 +5,7 @@ const sqsClient = new SQSClient({ region: 'us-east-1' })
 exports.sendReport = async (req) => {
   // const user = await new SessionApi(apiConfig).sessionGet(null, {headers: {cookie: req.header('cookie')}}).then(d => d.data)
   const id = req.params.id
-  const { Item } = await get({ id }, 'scheduler-1')
+  const { Item } = await get(id, 'scheduler-1')
   console.log('data', Item)
   sqsClient.send(new SendMessageCommand({
     MessageBody: JSON.stringify({ userId: Item.userId, items: [Item] }),
