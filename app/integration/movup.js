0cd78aa47c1d6b1b64c45d3a826b69b62812de8e
jcardus <asklocation.net@gmail.com>
2024-03-07 00:17:03 +0000

    timeout 4000

--- app/integration/movup.js
+++ app/integration/movup.js
@@ -17,6 +17,7 @@ module.exports = async (e) => {
   console.log('movup', data,
     await axios.post('https://segmentado.ziyu.cl/api/restapp/gpssignal/external/', data,
       {
+        timeout: 4000,
         headers: {
           Username: 'Fleetrack',
           Password: 'yYoHmGkE7mXj'
