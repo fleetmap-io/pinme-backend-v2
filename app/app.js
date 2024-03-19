cd5ce74edfca308f459481e86bfab0050a20980c
jcardus <asklocation.net@gmail.com>
2024-03-19 02:57:40 +0000

    remove custom attributes

--- app/app.js
+++ app/app.js
@@ -1,9 +1,8 @@
 const CognitoExpress = require('cognito-express')
 const { getOneSignalTokens } = require('fleetmap-partners')
 const { logException } = require('./utils')
-const { CognitoIdentityProviderClient, ListUsersCommand, AdminUpdateUserAttributesCommand } = require('@aws-sdk/client-cognito-identity-provider')
+const { CognitoIdentityProviderClient, ListUsersCommand } = require('@aws-sdk/client-cognito-identity-provider')
 const crypto = require('crypto')
-const axios = require('axios')
 
 exports.mainFunction = async (event) => {
   if (event.httpMethod === 'OPTIONS') {
@@ -38,43 +37,6 @@ exports.mainFunction = async (event) => {
       Limit: 1
     }))
     email = listUsersResponse.Users[0].Attributes.find(a => a.Name === 'email')
-    try {
-      let skip = false
-      const xforwarded = event.headers && event.headers['X-Forwarded-For']
-      if (xforwarded) {
-        try {
-          const skipCountries = ['PT', 'MA']
-          const { country } = await axios.get(`https://ipinfo.io/${xforwarded.split(',')[0]}?token=${process.env.IPINFO_TOKEN}`, { timeout: 1000 }).then(d => d.data)
-          if (skipCountries.indexOf(country) !== -1) {
-            skip = true
-            console.log(xforwarded, 'skipping for', country, email)
-          } else {
-            console.log('moving on for', country, email, xforwarded)
-          }
-        } catch (e) { console.error(e) }
-      }
-      if (!skip && !listUsersResponse.Users[0].Attributes.find(a => a.Name === 'custom:SERVER_HOST')) {
-        console.log('setting custom attributes', email)
-        await cognito.send(new AdminUpdateUserAttributesCommand({
-          UserPoolId: process.env.USER_POOL_ID,
-          Username: listUsersResponse.Users[0].Username,
-          UserAttributes: [ // AttributeListType // required
-            { // AttributeType
-              Name: 'custom:SERVER_HOST', // required
-              Value: 'api2.pinme.io'
-            },
-            { // AttributeType
-              Name: 'custom:WEB_SOCKET_HOST', // required
-              Value: 'traccar-eu.fleetmap.pt'
-            }
-          ]
-        }))
-      } else {
-        console.log(email, 'already has custom attributes')
-      }
-    } catch (e) {
-      console.error(e)
-    }
     const [cookies] = await (await require('./auth')).getUserSession(email.Value, crypto.randomUUID())
     return okResponse('', event, cookies)
   } catch (e) {
