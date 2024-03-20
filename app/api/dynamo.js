bafa29056865388531ed8777d6ffe7e1b6ff2fbe
jpenas <jppenas@gmail.com>
2024-03-20 22:49:17 +0000

    fix getAllSchedules

--- app/api/dynamo.js
+++ app/api/dynamo.js
@@ -1,4 +1,4 @@
-const { GetItemCommand, PutItemCommand, UpdateItemCommand, DeleteItemCommand, DynamoDBClient } = require('@aws-sdk/client-dynamodb')
+const { GetItemCommand, PutItemCommand, UpdateItemCommand, DeleteItemCommand, DynamoDBClient, ScanCommand } = require('@aws-sdk/client-dynamodb')
 const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb')
 const dynamo = new DynamoDBClient({ region: 'us-east-1' })
 const schedulerTable = 'scheduler-1'
@@ -20,55 +20,24 @@ exports.put = (device) => {
   return dynamo.send(put)
 }
 
-exports.getSchedules = async (user) => {
-  const next = new Date(Date.now())
-  next.setUTCHours(23, 59, 59, 59)
-  const last = new Date(Date.now())
-  last.setUTCHours(0, 0, 0, 0)
-  const params = {
-    TableName: schedulerTable,
-    FilterExpression: 'userId = :userId and (lastExecutionDate < :lastExecutionDate or attribute_not_exists(lastExecutionDate) or lastExecutionDate = :emptyLastExecutionDate) and (nextExecution <= :nextExecution or attribute_not_exists(nextExecution) or nextExecution = :nullNextExecution)',
-    ExpressionAttributeValues: {
-      ':userId': user.id,
-      ':nextExecution': next.toISOString(),
-      ':nullNextExecution': null,
-      ':lastExecutionDate': last.toISOString(),
-      ':emptyLastExecutionDate': ''
-    }
-  }
-  return await scan(params)
-}
-
-async function scan (params) {
-  let lastEvaluatedKey = null
-  let result = []
-  do {
-    if (lastEvaluatedKey) {
-      params.ExclusiveStartKey = lastEvaluatedKey
-    }
-    const data = await scan(params).promise()
-    result = result.concat(data.Items)
-    lastEvaluatedKey = data.LastEvaluatedKey
-  } while (lastEvaluatedKey)
-  return result
-}
-
 exports.getAllSchedules = async () => {
   const next = new Date(Date.now())
   next.setUTCHours(23, 59, 59, 59)
   const last = new Date(Date.now())
   last.setUTCHours(0, 0, 0, 0)
-  const params = {
-    TableName: schedulerTable,
-    FilterExpression: '(lastExecutionDate < :lastExecutionDate or attribute_not_exists(lastExecutionDate) or lastExecutionDate = :emptyLastExecutionDate) and (nextExecution <= :nextExecution or attribute_not_exists(nextExecution) or nextExecution = :nullNextExecution)',
-    ExpressionAttributeValues: {
-      ':lastExecutionDate': last.toISOString(),
-      ':emptyLastExecutionDate': '',
-      ':nextExecution': next.toISOString(),
-      ':nullNextExecution': null
-    }
+  const expression = '(lastExecutionDate < :lastExecutionDate or attribute_not_exists(lastExecutionDate) or lastExecutionDate = :emptyLastExecutionDate) and (nextExecution <= :nextExecution or attribute_not_exists(nextExecution) or nextExecution = :nullNextExecution)'
+  const values = {
+    ':lastExecutionDate': marshall(last.toISOString()),
+    ':emptyLastExecutionDate': '',
+    ':nextExecution': marshall(next.toISOString()),
+    ':nullNextExecution': null
   }
-  return await scan(params)
+  const data = await dynamo.send(new ScanCommand({
+    TableName: schedulerTable,
+    FilterExpression: expression,
+    ExpressionAttributeValues: values
+  }))
+  return data.Items.map(i => unmarshall(i))
 }
 
 exports.updateSchedule = async (id, userId, nextExecutionDate) => {
