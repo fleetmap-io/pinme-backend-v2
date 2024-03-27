10aa1525f1710adf93d4da06fcee46efbbb47ed5
jcardus <asklocation.net@gmail.com>
2024-03-27 02:23:10 -0100

    tests

--- app/tests/gpsmanager/test.js
+++ app/tests/gpsmanager/test.js
@@ -1,8 +1,10 @@
-const devices = require('../../devices')
+const gpsmanager = require('../../gpsmanager')
 
+// eslint-disable-next-line no-undef
 describe('GPS Manager', () => {
-  it('should create a user with the device', async () => {
-        const result = await gpsmanager.main(require('../../../events/gpsmanager.json'))
-        console.log(result)
-  });
-});
+  // eslint-disable-next-line no-undef
+  it('should list sms received', async () => {
+    const result = await gpsmanager.main(require('./sentsms.json'))
+    console.log('resul', result)
+  })
+})
